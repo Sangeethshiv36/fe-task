2fb6c469feab15ebe234ba9ac2ac2ae62ac6d785
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-25 05:08:31 +0530

    refactored app js to be more readable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
+import StateContext from './StateContext';
+import DispatchContext from './DispatchContext';
+import { useImmerReducer } from 'use-immer';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Container from './components/Container';
+import ContactsPage from './components/ContactsPage';
+
 function App() {
+  const initialState = {
+    activeUser: {
+      userName: 'John Doe'
+    },
+    isDropdownOpen: false
+  };
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case 'SET_ACTIVE_USER':
+        draft.activeUser = action.data;
+        break;
+      case 'TOGGLE_DROPDOWN':
+        draft.isDropdownOpen = !draft.isDropdownOpen;
+        break;
+      default:
+        break;
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
   return (
-    <div className="App">
-      <p>Progress</p>
-    </div>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <Container>
+          <Header />
+          <Sidebar />
+          <ContactsPage />
+        </Container>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   );
 }
 

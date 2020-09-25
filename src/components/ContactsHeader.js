06ed56f66f8ecd8a1d4b9b80d3b6775a8b6e9b3b
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-25 15:22:28 +0530

    contacts header component

---
+++ src/components/ContactsHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SearchInput from './SearchInput';
+
+function ContactsHeader() {
+  return (
+    <>
+      <div className="top-content">
+        <div class="ct-header">
+          <h1>Contacts</h1>
+          <sub>Welcome to FlatCRM Contact page</sub>
+        </div>
+        <div className="sort-contacts">
+          <span>
+            Sort by: <strong>Date Created </strong>
+          </span>
+        </div>
+      </div>
+      <div className="bottom-content">
+        <SearchInput />
+        <button className="add-ct-btn">
+          <strong> + Add Contact</strong>
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ContactsHeader;

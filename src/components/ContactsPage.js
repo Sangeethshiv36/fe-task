0a7d2f8670185e58aae7f05661f2d83ab87febe7
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-25 18:05:44 +0530

    pass userdata to contact view

--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContactsList from './ContactsList';
 import ContactView from './ContactView';
 import ContactsHeader from './ContactsHeader';
 
 function ContactsPage() {
+  const [userInfo, setUserInfo] = useState([]);
   return (
     <div className="contacts-page">
       <div className="contacts-header">
         <ContactsHeader />
       </div>
       <div className="contacts-content">
-        <ContactsList />
-        <ContactView />
+        <ContactsList setUserInfo={setUserInfo} />
+        <ContactView userInfo={userInfo && userInfo} />
       </div>
     </div>
   );

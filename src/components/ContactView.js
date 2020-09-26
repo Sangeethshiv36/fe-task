bb9f2a1c6ba974a2a3ae01a77e0cfafd7295ac8f
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-26 08:17:10 +0530

    view page

--- src/components/ContactView.js
+++ src/components/ContactView.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import ProfileInfo from './ProfileInfo';
 
 function ContactView({ userInfo }) {
   if (userInfo) {
+    const name = `${userInfo.first_name} ${userInfo.last_name}`;
     return (
       <div className="contacts-view">
-        <div></div>
+        <div className="contact-detail">
+          {
+            <ProfileInfo
+              name={name}
+              role={userInfo.role}
+              company={userInfo.company}
+              color={userInfo.profileColor}
+              view="column"
+            />
+          }
+          <div className="user-details"></div>
+        </div>
+        <div className="notices"></div>
       </div>
     );
   } else {

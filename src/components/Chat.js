7e2af06da55f5a844b83aa5dd54fe90bb8222aa8
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-26 18:17:41 +0530

    Chat component WIP

---
+++ src/components/Chat.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useContext } from 'react';
+import StateContext from '../StateContext';
+import DispatchContext from '../DispatchContext';
+import { useImmer } from 'use-immer';
+
+function Chat({ toUser }) {
+  const chatField = useRef(null);
+  const chatLog = useRef(null);
+
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+
+  const [state, setState] = useImmer({
+    fieldValue: '',
+    chatMessages: []
+  });
+
+  function handleFieldChange(e) {
+    const { value } = e.target;
+    setState((draft) => {
+      draft.fieldValue = value;
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const time = Date.now();
+    setState((draft) => {
+      draft.chatMessages.push({
+        sentMessage: {
+          to: toUser,
+          from: appState.activeUser.id,
+          message: draft.fieldValue,
+          time: time
+        },
+        recievedMessage: {
+          from: '',
+          to: appState.activeUser.id,
+          message: '',
+          time: time
+        },
+        userName: appState.activeUser.userName,
+        userId: appState.activeUser.id
+      });
+      draft.fieldValue = '';
+    });
+  }
+
+  useEffect(() => {
+    if (appState.isChatOpen) {
+      chatField.current.focus();
+    }
+  }, [appDispatch, appState.isChatOpen]);
+
+  return (
+    <div
+      id="chat-wrapper"
+      className={`chat-wrapper  shadow border-top border-left border-right ${
+        appState.isChatOpen ? 'chat-wrapper--is-visible' : ''
+      }`}
+    >
+      <div className="chat-title-bar">
+        Chat
+        <button
+          onClick={() => {
+            appDispatch({ type: 'CLOSE_CHAT' });
+          }}
+          className="icon-button nav-button chat-close-icon"
+        >
+          <svg className="_modal-close-icon" viewBox="0 0 40 40">
+            <path d="M 10,10 L 30,30 M 30,10 L 10,30" />
+          </svg>
+        </button>
+      </div>
+      <div id="chat" ref={chatLog} className="chat-log">
+        {state.chatMessages.map((message, index) => {
+          if (message.userId === appState.activeUser.id) {
+            console.log('in');
+            return (
+              <div key={index} className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">
+                    {message.sentMessage && message.sentMessage.message}
+                  </div>
+                </div>
+              </div>
+            );
+          }
+
+          return (
+            <div key={index} className="chat-other">
+              <div className="chat-message">
+                <div className="chat-message-inner">
+                  {message.receievedMessage && message.recievedMessage.message}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        id="chatForm"
+        className="chat-form border-top"
+      >
+        <input
+          value={state.fieldValue}
+          onChange={handleFieldChange}
+          ref={chatField}
+          type="text"
+          className="chat-field"
+          id="chatField"
+          placeholder="Type a message…"
+          autoComplete="off"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Chat;

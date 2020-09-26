68b33529a35315aebf4503734f585782dd44a20f
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-26 18:17:22 +0530

    import chat and added more state data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import StateContext from './StateContext';
 import DispatchContext from './DispatchContext';
@@ -8,19 +8,25 @@ import Sidebar from './components/Sidebar';
 import Container from './components/Container';
 import ContactsPage from './components/ContactsPage';
 
+const Chat = React.lazy(() => import('./components/Chat'));
+
 function App() {
   const initialState = {
     usersList: [],
     activeUser: {
-      userName: 'John Doe'
+      userName: 'John Doe',
+      id: 161
     },
-    isDropdownOpen: false
+    isDropdownOpen: false,
+    isChatOpen: false,
+    toChatId: ''
   };
 
   function ourReducer(draft, action) {
     switch (action.type) {
       case 'SET_ACTIVE_USER':
-        draft.activeUser.userName = action.data;
+        draft.activeUser.userName = action.data.userName;
+        draft.activeUser.id = action.data.id;
         break;
       case 'TOGGLE_DROPDOWN':
         draft.isDropdownOpen = !draft.isDropdownOpen;
@@ -28,13 +34,35 @@ function App() {
       case 'SET_USERS_LIST':
         draft.usersList = action.data;
         break;
+      case 'CONTACT_ADDED':
+        draft.usersList.push(action.data);
+        break;
+      case 'CONTACT_EDITED':
+        if (action.data) {
+          const userIndex = draft.usersList.findIndex(
+            (user) => user.id === action.data.id
+          );
+          draft.usersList[userIndex] = {
+            id: action.data.id,
+            gender: action.data.gender,
+            profileColor: action.data.profileColor,
+            ...action.data.updatedContact
+          };
+        }
+        break;
+      case 'TOGGLE_CHAT':
+        draft.isChatOpen = !draft.isChatOpen;
+        draft.toChatId = action.data;
+        break;
+      case 'CLOSE_CHAT':
+        draft.isChatOpen = !draft.isChatOpen;
+        break;
       default:
         break;
     }
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, initialState);
-
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
@@ -42,6 +70,9 @@ function App() {
           <Header />
           <Sidebar />
           <ContactsPage />
+          <Suspense fallback="">
+            <Chat toUser={state.toChatId && state.toChatId} />
+          </Suspense>
         </Container>
       </DispatchContext.Provider>
     </StateContext.Provider>

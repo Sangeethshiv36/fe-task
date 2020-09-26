9225b0d97010d170c03fffb5d82c3e8c367f229a
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-26 08:17:27 +0530

    profile info updated to handle contact view

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-function ProfileInfo({ name, email, color }) {
+function ProfileInfo({ name, email, color, role, company, view }) {
   const initials = `${name.split(' ')[0][0]} ${name.split(' ')[1][0]}`;
   return (
     <>
-      <div className="contact-info">
+      <div
+        className={view === 'column' ? 'contact-info view-col' : 'contact-info'}
+      >
         <span className="initials" style={{ backgroundColor: `${color}` }}>
           <strong>{initials}</strong>
         </span>
         <div className="user-info">
           <h3>{name}</h3>
           <sub>{email}</sub>
+          {role && company && (
+            <div className="work-info">
+              <sub>{role}</sub>
+              <sub>@{company}</sub>
+            </div>
+          )}
         </div>
       </div>
     </>

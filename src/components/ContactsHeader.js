232656e6ad599c1274e0aa75e648e47495277d1e
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-26 18:18:10 +0530

    contact header component open add contacts modal

--- src/components/ContactsHeader.js
+++ src/components/ContactsHeader.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SearchInput from './SearchInput';
+import ModalContainer from './ModalContainer';
+import DispatchContext from '../DispatchContext';
 
 function ContactsHeader() {
+  const triggerText = `+ Add Contacts`;
+  const apPDispatch = useContext(DispatchContext);
+
+  function onSubmit(event) {
+    event.preventDefault(event);
+    const target = event.target;
+    const nameArr = target.name.value.split(' ');
+    const ctObj = {
+      id: Math.floor(Math.random() * 1000),
+      first_name: nameArr[0],
+      last_name: nameArr[1] ? nameArr[1] : '',
+      email: target.email.value,
+      phone: target.phone.value,
+      role: target.role.value,
+      company: target.company.value,
+      country: target.country.value,
+      profileColor: '#783927'
+    };
+    apPDispatch({ type: 'CONTACT_ADDED', data: ctObj });
+  }
+
+  // const getInitials = (name) => {
+  //   const nameArr = name.split(' ');
+  //   if (nameArr.length > 1) {
+  //     return `${nameArr[0][0]}``${nameArr[1][0]}`;
+  //   } else {
+  //     return `${nameArr[0][0]}`;
+  //   }
+  // };
+
   return (
     <>
       <div className="top-content">
@@ -17,9 +49,12 @@ function ContactsHeader() {
       </div>
       <div className="bottom-content">
         <SearchInput />
-        <button className="add-ct-btn">
-          <strong> + Add Contact</strong>
-        </button>
+        <ModalContainer
+          classType="button"
+          mode="add"
+          triggerText={triggerText}
+          onSubmit={onSubmit}
+        />
       </div>
     </>
   );
